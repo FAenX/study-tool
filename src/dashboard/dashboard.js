337d9e987811a0a760ac67b92c9fc4a36331ab88
emmanuel <kipronofb@gmail.com>
2020-01-12 04:30:07 +0300

    commit

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -7,7 +7,7 @@ class History extends React.Component{
         
         return(
             <div>
-                <Card variant="elevation" elevation={2} id="history" className="stats-item">
+                <Card variant="elevation" elevation={1} id="history" className="stats-item">
                 <Card variant="elevation" elevation={5} className="stats-item-header">Study history</Card>
                     {this.props.done} cycles
                 </Card>
@@ -22,7 +22,7 @@ class DataViz extends React.Component{
     render(){
         return(
             <div>
-                <Card variant="elevation" elevation={2} id="dataviz" className="stats-item">
+                <Card variant="elevation" elevation={1} id="dataviz" className="stats-item">
                 <Card variant="elevation" elevation={5} className="stats-item-header">Visualize Your Daily Progress</Card>
 
 
@@ -37,7 +37,7 @@ class TableSettings extends React.Component{
     render(){
         return(
             <div>
-                <Card variant="elevation" elevation={2} id="table-settings" className="stats-item">
+                <Card variant="elevation" elevation={1} id="table-settings" className="stats-item">
                 <Card variant="elevation" elevation={5} className="stats-item-header">Make adjustments to the table</Card>
 
 
