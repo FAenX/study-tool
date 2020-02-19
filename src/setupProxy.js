4b916a0d7f4ecb874ecad006297d92eac9161995
emmanuel <kipronofb@gmail.com>
2020-02-19 23:27:39 +0300

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,13 @@
+
+
 import proxy from "http-proxy-middleware";
 
 
-  export default function(app) {  
-    app.use(proxy("/api/v1/**", 
-    { target: "http://young-tundra-77987.herokuapp.com/", changeOrigin: true, "secure": false, "logLevel": "debug"}));
+export default function(app) {  
+    app.use(proxy("/api", 
+      { target: "https://young-tundra-77987.herokuapp.com/", 
+        changeOrigin: true, 
+      }
+    ));
   };
 
